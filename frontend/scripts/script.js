c882f4e4b4324e199a2f1c6035b04f6eb6bb4077
Stéphanie Cândido <stephanie.candido42@gmail.com>
2024-07-09 04:21:14 -0300

    feat: basic landing page for the API

---
+++ frontend/scripts/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const city = 'Recife';
+    fetch(`http://localhost:3000/points/city/${city}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const resultList = document.getElementById('result-list');
+            resultList.innerHTML = '';
+            data.forEach(point => {
+                const listItem = document.createElement('li');
+                let listItemHTML = `
+                    <p><strong>Nome:</strong> ${point.name}</p>
+                    <p><strong>Endereço:</strong> ${point.street_address || 'N/A'}</p>
+                    <p><strong>Tipo:</strong> <span class="type">${point.type}</span></p>
+                    <p><strong>Contato:</strong> ${point.contact || 'N/A'}</p>
+                `;
+                listItem.innerHTML = listItemHTML.trim();
+                resultList.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error('Erro ao buscar pontos de coleta:', error));
+});
